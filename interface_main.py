91634abc92a1fac2b85c6355f8a48005e2927487
Lisa <lisa@MacBook-Air-de-Lisa.local>
2024-03-24 14:41:24 +0100

    ajout d'un panier de selection

--- interface_main.py
+++ interface_main.py
@@ -29,34 +29,43 @@ output_dir = "images_reconstruites"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+# with torch.no_grad():
+#     input_batch = next(iter(data_loader))
+#
+# # Reconstruire les images à partir du modèle
+# #global recon_batch
+# recon_batch, _, _ = autoencoder(input_batch)
+#
+# # Convertir les tenseurs PyTorch en numpy arrays
+# input_batch = input_batch.numpy()
+# recon_batch = recon_batch.detach().numpy()
+
 # # Supprimer les images après utilisation (facultatif)
 # for i in range(n):
 #     img_path = os.path.join(output_dir, f"reconstructed_image_{i}.jpg")
 #     os.remove(img_path)
 
+global IMG_COORDS
+IMG_COORDS = []
+global PHOTOS
+PHOTOS = []
+#print(len(PHOTOS))
+#print(len(IMG_COORDS))
 
 # Fonction appelée lors du clic sur le bouton "Créer un portrait robot"
 def create_portrait():
     for widget in center_frame.winfo_children():
          widget.destroy()
-    # Supposons que la fonction génère trois images, nous allons simplement créer des libellés pour les afficher
-    # Remplace cette logique avec ta propre logique de génération d'images
-    leftside_frame.pack_forget()
-    #label2_welcome.destroy()
-    #button2_create.pack_forget()
-    logo_label.pack_forget()
 
+    # Proposer desimages décodées
     with torch.no_grad():
         input_batch = next(iter(data_loader))
-
     # Reconstruire les images à partir du modèle
     global recon_batch
     recon_batch, _, _ = autoencoder(input_batch)
-
     # Convertir les tenseurs PyTorch en numpy arrays
     input_batch = input_batch.numpy()
     recon_batch = recon_batch.detach().numpy()
-
 
     n = 10  # Nombre d'images à afficher
     # Enregistrer les images reconstruites au format JPG avec une correction des niveaux de gris
@@ -66,6 +75,9 @@ def create_portrait():
         img = Image.fromarray(img_array.reshape(64, 64))
         img_path = os.path.join(output_dir, f"photo{i}.jpg")  # Utilisation de f-strings
         img.save(img_path, quality=90)
+        IMG_COORDS.append(img)
+    #print(len(IMG_COORDS))
+
 
     global selected_photos
     selected_photos = []
@@ -111,14 +123,89 @@ def create_portrait():
     button_create.pack_forget()
 
     # Affichage des boutons "Continuer" et "Terminer"
-    button_continue = tk.Button(center_frame, text="Continuer", command=continue_selection)
-    button_finish = tk.Button(center_frame, text="Terminer", command=finish_selection)
+    button_continue = tk.Button(center_frame, text="Continuer la sélection", command=continue_selection)
     button_continue.pack(side=tk.BOTTOM, padx=60, pady=10)
-    button_finish.pack(side=tk.BOTTOM, padx=60, pady=10)
+    global button_panier
+    button_panier = tk.Button(text="Photos sélectionnées", command=photos_selectioned)
+    button_panier.place(relx=1.0, rely=0.0, anchor='ne', bordermode='outside', x=-30, y=150)
+    button_newfaces = tk.Button(center_frame, text="Individus proposés", command=new_faces)
+    button_newfaces.pack(side=tk.BOTTOM, padx=60, pady=10)
+    #button_finish = tk.Button(center_frame, text="Terminer", command=finish_selection)
+    #button_finish.pack(side=tk.BOTTOM, padx=60, pady=10)
 
+def photos_selectioned():
+    for widget in center_frame.winfo_children():
+         widget.destroy()
+    label_explication = tk.Label(center_frame, text="Mes photos sélectionnées : ",bg="white",font=("Helvetica", 30))
+    label_explication.pack(padx=20,pady=10,fill=tk.X)
+
+    i=1
+    for img in PHOTOS:
+        img_path = os.path.join("images_selectionnees", f"photo{i}.jpg")  # Utilisation de f-strings
+        img.save(img_path, quality=90)
+        i+=1
+
+    photo_list = []  # Liste pour stocker les objets PhotoImage
+    n=len(PHOTOS)
+    # Charger les images reconstruites
+    for i in range(1,n+1):
+        img_path = os.path.join("images_selectionnees", f"photo{i}.jpg")
+        photo = ImageTk.PhotoImage(Image.open(img_path))
+        photo_list.append(photo)
+
+    photo_checkboxes = []
+    # Affichage des photos dans la fenêtre principale
+    if n<=1:
+        photos_per_row = 1
+        row_count = 1
+    else :
+        photos_per_row = n // 2  # Nombre de photos par ligne
+        row_count = (n + photos_per_row - 1) // photos_per_row  # Nombre total de lignes
+    for row_index in range(row_count):
+        row_frame = tk.Frame(center_frame)  # Créer un nouveau cadre pour chaque ligne
+        row_frame.pack()  # Pack le cadre de la ligne
+        for j in range(photos_per_row):
+            i = row_index * photos_per_row + j  # Calcul de l'index de la photo
+            if i >= n:  # Si nous avons dépassé le nombre total de photos, sortir de la boucle
+                break
+            photo = photo_list[i]
+            photo_id = i+1
+            checkbox_var = tk.BooleanVar()
+            checkbox = tk.Checkbutton(row_frame, image=photo, variable=checkbox_var, onvalue=True, offvalue=False, command=lambda p=photo_id: toggle_photo(p))
+            checkbox.image = photo
+            checkbox.pack(side=tk.LEFT, padx=10, pady=10)
+            photo_checkboxes.append(checkbox)
+
+    global button_retour
+    button_retour = tk.Button(text="Retour", command=retour)
+    button_retour.place(relx=1.0, rely=0.0, anchor='ne', bordermode='outside', x=-30, y=150)
+    # Effacer le bouton button_panier
+    if button_panier:
+        button_panier.destroy()
+
+def retour():
+    button_retour.destroy()
+    for widget in center_frame.winfo_children():
+         widget.destroy()
+    button_panier = tk.Button(text="Photos sélectionnées", command=photos_selectioned)
+    button_panier.place(relx=1.0, rely=0.0, anchor='ne', bordermode='outside', x=-30, y=150)
+    # Affichage des boutons "Continuer" et "Terminer"
+    #button_continue = tk.Button(center_frame, text="Continuer", command=continue_selection)
+    #button_finish = tk.Button(center_frame, text="Terminer", command=finish_selection)
+    #button_continue.pack(side=tk.BOTTOM, padx=60, pady=10)
+    #button_finish.pack(side=tk.BOTTOM, padx=60, pady=10)
+
+def continue_selection():
+    for widget in center_frame.winfo_children():
+         widget.destroy()
+    i=len(IMG_COORDS)-10
+    for x in selected_photos:
+        x+=i
+        PHOTOS.append(IMG_COORDS[x-1])
+    create_portrait()
 
 # Fonction appelée lors du clic sur le bouton "Continuer"
-def continue_selection():
+def new_faces():
     global selected_photos_continue
 
     #print(selected_photos)
@@ -131,7 +218,7 @@ def continue_selection():
         for widget in center_frame.winfo_children():
              widget.destroy()
         # Fais quelque chose avec les paramètres du décodeur, par exemple :
-        image_coords = [recon_batch[1,0,0].flatten()]
+        image_coords = [recon_batch[1,0,0].flatten(),recon_batch[2,0,0].flatten()]
         image_coords = np.array(image_coords)
         latent_coordinates = torch.tensor(image_coords)
         generated_image = autoencoder.decoder(latent_coordinates)
@@ -206,9 +293,9 @@ def continue_selection():
         button_create.pack_forget()
 
         # Affichage des boutons "Continuer" et "Terminer"
-        button_continue = tk.Button(center_frame, text="Continuer", command=continue_selection)
+        button_newfaces = tk.Button(center_frame, text="Continuer", command=new_faces)
+        button_newfaces.pack(side=tk.BOTTOM, padx=60, pady=10)
         button_finish = tk.Button(center_frame, text="Terminer", command=finish_selection)
-        button_continue.pack(side=tk.BOTTOM, padx=60, pady=10)
         button_finish.pack(side=tk.BOTTOM, padx=60, pady=10)
         #create_portrait()
 
@@ -225,9 +312,7 @@ def finish_selection():
         if len(selected_photos)>1 or len(selected_photos_continue)>1:
             label_comment = tk.Label(center_frame, text="Error : Appuyer sur Continuer", font=("Helvetica", 16), bg="red")
             label_comment.pack()
-        # Créer un bouton "Retour"
-        #button_back = tk.Button(center_frame, text="Retour", command=back_to_selection)
-        #button_back.pack()
+
     else :
         for widget in center_frame.winfo_children():
             widget.destroy()
@@ -257,6 +342,12 @@ def back_to_selection():
     button2_create.pack(pady=5)
     #create_portrait()
 
+def method():
+    for widget in center_frame.winfo_children():
+         widget.destroy()
+    label_explication = tk.Label(center_frame, text="Explications : ",bg="white",font=("Helvetica", 30))
+    label_explication.pack(padx=20,pady=10,fill=tk.X)
+
 # Création de la fenêtre principale
 root = tk.Tk()
 root.title("https://mon-protrait-robot.com")
@@ -279,9 +370,9 @@ label_title = tk.Label(title_frame, text="Créateur de portraits robots", font=(
 label_title.pack()
 
 # Ajout d'un cadre bleu sur le bord droit de l'écran
-leftside_frame = tk.Frame(root, bg="#000080", width=200, height=screen_height)
-leftside_frame.pack(fill=tk.Y, pady=0, side=tk.LEFT)
-leftside_frame.pack_propagate(0)
+#leftside_frame = tk.Frame(root, bg="#000080", width=200, height=screen_height)
+#leftside_frame.pack(fill=tk.Y, pady=0, side=tk.LEFT)
+#leftside_frame.pack_propagate(0)
 
 # Création d'un cadre pour la zone centrale
 center_frame = tk.Frame(root,bg="white",bd=5)
@@ -293,18 +384,22 @@ label_welcome.pack(padx=20,pady=10,fill=tk.X)
 
 # Créer un bouton pour créer un portrait robot
 button_create = tk.Button(center_frame, text="Créer un portrait robot", command=create_portrait,foreground="black")#,font=("Helvetica", 15))
-button_create.pack(pady=5)
+button_create.pack(pady=25)
+
+button_method = tk.Button(center_frame, text="Méthodes : ", command=method,foreground="black")#,font=("Helvetica", 15))
+button_method.pack(pady=5)
+
 
 label2_welcome = tk.Label(center_frame, text="Bienvenue dans le créateur de portraits robots !",bg="white",font=("Helvetica", 30))
 button2_create = tk.Button(center_frame, text="Créer un portrait robot", command=create_portrait,foreground="black")#,font=("Helvetica", 15))
 
 # Chargement de l'image du logo
-logo_image = Image.open("logo.png")  # Remplacez "logo.png" par le chemin de votre fichier logo
-logo_photo = ImageTk.PhotoImage(logo_image)
+#logo_image = Image.open("logo.png")  # Remplacez "logo.png" par le chemin de votre fichier logo
+#logo_photo = ImageTk.PhotoImage(logo_image)
 
 # Création d'un label pour afficher le logo
-logo_label = tk.Label(root, image=logo_photo, bg="white")
-logo_label.pack(side=tk.RIGHT, padx=10, pady=10, anchor="nw")  # Placer le logo dans le coin en haut à gauche
+#logo_label = tk.Label(root, image=logo_photo, bg="white")
+#logo_label.pack(side=tk.RIGHT, padx=10, pady=10, anchor="nw")  # Placer le logo dans le coin en haut à gauche
 
 
 # Lancement de la boucle principale de l'interface graphique
